ec73419b4f734556aea642498aee2fdd6eef53fb
YAYA ABDEL AZIZ DIARRA <aziz.diarra@ngser.com>
2025-12-24 09:26:45 +0000

    niveau

--- src/niveau/dto/create-niveau.dto.ts
+++ src/niveau/dto/create-niveau.dto.ts
@@ -1,8 +1,10 @@
-import { IsString } from "class-validator";
+import { IsOptional, IsString } from "class-validator";
 
 export class CreateNiveauDto {
     @IsString()
     nomNiveau:string;
+
+    @IsOptional()
     @IsString()
     membreCo:string;
 }
